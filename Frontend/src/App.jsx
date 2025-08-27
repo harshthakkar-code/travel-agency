660cda35ac44803fd51f17f21ed5c7519f5fd1e5
harshthakkar-code <harryparkar111@gmail.com>
2025-08-27 14:48:31 +0530

    App changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './about.jsx';
 import Contact from './contact.jsx';
-import Service from './service.jsx';
-import FAQ from './faq.jsx';
 import Destination from './destination.jsx';
 import TourPackages from './tour-packages.jsx';
 import PackageDetail from './package-detail.jsx';
@@ -10,30 +8,13 @@ import Booking from './booking.jsx';
 import Gallery from './gallery.jsx';
 import Page404 from './404.jsx';
 import Blogarchive from './blog-archive.jsx';
-import BlogArchiveLeft from './BlogArchiveLeft.jsx';
 import Blog_single from './blog-single.jsx';
-import BlogArchiveBoth from './blog-archive-both.jsx';
-import TourCart from './tour-cart.jsx';
-import TourGuide from './tour-guide.jsx';
-import SearchPage from './search-page.jsx';
-import CareerDetails from './career-detail.jsx';
-import Career from './career.jsx';
-import CommingSoon from './comming-soon.jsx';
 import Confirmation from './confirmation.jsx';
 import Single_page from './single-page.jsx';
-import Testimonial_page from './testimonial-page.jsx';
 import Wishlist_page from './wishlist-page.jsx';
-import ProductCheckout from './product-checkout.jsx';
-import ProductDetail from './product-detail.jsx';
-import ProductCart from './product-cart.jsx';
-import ProductRight from './product-right.jsx';
-import Popup from './popup.jsx';
 import PackageOffer from './package-offer.jsx';
-import Index_v2 from './index-v2.jsx';
 // Admin imports
 import DbPackagePending from './admin/db-package-pending.jsx';
-import DbPackage from './admin/db-package.jsx';
-import DbWishlist from './admin/db-wishlist.jsx';
 import Forgot from './admin/forgot.jsx';
 import Login from './admin/login.jsx';
 import NewUser from './admin/new-user.jsx';
@@ -41,9 +22,7 @@ import UserEdit from './admin/user-edit.jsx';
 import User from './admin/user.jsx';
 import DbAddPackage from './admin/db-add-package.jsx';
 import Dashboard from './admin/dashboard.jsx';
-import DbBooking from './admin/db-booking.jsx';
 import DbPackageExpired from './admin/db-package-expired.jsx';
-import DbComment from './admin/db-comment.jsx';
 import DbPackageActive from './admin/db-package-active.jsx';
 import DbEditPackage from './admin/DbEditPackage.jsx';
 import Register from './admin/register.jsx';
@@ -60,33 +39,16 @@ function App() {
       <Route path="/" element={<TourPackages />} />
       <Route path="/about" element={<About />} />
       <Route path='/blog-archive' element={<Blogarchive />} />
-      <Route path='/blog-archive-left' element={<BlogArchiveLeft />} />
       <Route path='/blog-single/:id' element={<Blog_single />} />
       <Route path="/booking" element={<Booking />} />
-      <Route path='/blog-archive-both' element={<BlogArchiveBoth />} />
-      <Route path='/tour-cart' element={<TourCart />} />
-      <Route path='/index-v2' element={<Index_v2 />} />
-      <Route path='/tour-guide' element={<TourGuide />} />
-      <Route path='/search-page' element={<SearchPage />} />
-      <Route path='/career-details' element={<CareerDetails />} />
-      <Route path='/career' element={<Career />} />
-      <Route path='/coming-soon' element={<CommingSoon />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/confirmation" element={<Confirmation />} />
       <Route path="/gallery" element={<Gallery />} />
-      <Route path="/popup" element={<Popup />} />
-      <Route path="/product-checkout" element={<ProductCheckout />} />
-      <Route path="/product-detail" element={<ProductDetail />} />
-      <Route path="/product-cart" element={<ProductCart />} />
-      <Route path="/product-right" element={<ProductRight />} />
       <Route path="/tour-packages" element={<TourPackages />} />
       <Route path="/package-offer" element={<PackageOffer />} />
       <Route path="/package-detail/:id" element={<PackageDetail />} />
-      <Route path="/service" element={<Service />} />
-      <Route path="/faq" element={<FAQ />} />
       <Route path="/destination" element={<Destination />} />
       <Route path="/single-page" element={<Single_page />} />
-      <Route path="/testimonial-page" element={<Testimonial_page />} />
 
       {/* -------- User & Admin Accessible Routes -------- */}
       <Route path="/wishlist-page" element={
@@ -116,16 +78,6 @@ function App() {
           <DbPackagePending />
         </RequireAuth>
       } />
-      <Route path="/admin/db-package" element={
-        <RequireAuth allowedRoles={["admin"]}>
-          <DbPackage />
-        </RequireAuth>
-      } />
-      <Route path="/admin/db-wishlist" element={
-        <RequireAuth allowedRoles={["admin"]}>
-          <DbWishlist />
-        </RequireAuth>
-      } />
       <Route path="/admin/new-user" element={
         <RequireAuth allowedRoles={["admin"]}>
           <NewUser />
@@ -146,19 +98,9 @@ function App() {
           <DbAddPackage />
         </RequireAuth>
       } />
-      <Route path="/admin/db-booking" element={
-        <RequireAuth allowedRoles={["admin"]}>
-          <DbBooking />
-        </RequireAuth>
-      } />
       <Route path="/admin/db-package-expired" element={
         <RequireAuth allowedRoles={["admin"]}>
           <DbPackageExpired />
-        </RequireAuth>
-      } />
-      <Route path="/admin/db-comment" element={
-        <RequireAuth allowedRoles={["admin"]}>
-          <DbComment />
         </RequireAuth>
       } />
       <Route path="/admin/db-package-active" element={
