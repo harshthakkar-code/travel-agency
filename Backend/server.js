a1d7a221ccf85deadcc1a748a0fc38cb99680e8f
harshthakkar-code <harryparkar111@gmail.com>
2025-11-04 16:57:16 +0530

    start path added changes

--- Backend/server.js
+++ Backend/server.js
@@ -47,6 +47,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Travel Agency Backend API');
+});
+
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
