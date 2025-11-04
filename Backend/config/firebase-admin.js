058374556c8d56ca79b6fb2899a67cf3c98d0514
harshthakkar-code <harryparkar111@gmail.com>
2025-11-04 16:32:19 +0530

    changes

--- Backend/config/firebase-admin.js
+++ Backend/config/firebase-admin.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require('../config/firebase-service-account.json');
+// const serviceAccount = require('../config/firebase-service-account.json');
+const serviceAccount = require(path.join(process.cwd(), 'firebase-service-account.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
